0d4e702a88b1fb823a4b587756c20558ad81fc78
tssvetiina <c.nikolaeva2020@pmg-pernik.com>
2024-12-19 11:06:51 +0200

    place2

---
+++ WebShopApp/WebShopApp.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+using WebShopApp.Infrastructure.Data.Domain;
+
+namespace WebShopApp.Infrastructure.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
+
+
